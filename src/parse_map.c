4328d2239d9695cc6ce76835f06abb6e7d019375
Teemu Spoof <tspoof@student.hive.fi>
2023-08-31 19:24:37 +0300

    Added the map tests

--- src/parse_map.c
+++ src/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 14:31:07 by tspoof            #+#    #+#             */
-/*   Updated: 2023/08/31 14:39:06 by tspoof           ###   ########.fr       */
+/*   Updated: 2023/08/31 19:20:52 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 
 int	parse_map(char *path)
 {
-	(void)path;
+	int	fd;
+
+	fd = open(path, O_RDONLY);
+	// printf("(%s)\n", get_next_line(fd));
+	close(fd);
 	return (1);
 }
--- test/tests/test_parse_map.c
+++ test/tests/test_parse_map.c
@@ -1,15 +1,63 @@
 #include "cub3d.h"
 #include "unity.h"
+#include <dirent.h>
 
 void	test_bad(void)
 {
-	TEST_ASSERT_EQUAL_INT_MESSAGE(1, parse_map("maps/color_invalid_rgb.cup"),
-		"#1");
+	DIR				*d;
+	struct dirent	*dir;
+	char			*path;
+	char			*message;
+	int				i;
+
+	d = opendir("maps/bad/");
+	if (d)
+	{
+		i = 0;
+		while ((dir = readdir(d)) != NULL)
+		{
+			if (dir->d_type == DT_REG)
+			{
+				asprintf(&path, "maps/bad/%s", dir->d_name);
+				asprintf(&message, "#%d", i);
+				TEST_ASSERT_EQUAL_INT_MESSAGE(1, parse_map(path), message);
+				// printf("%s - %s\n", path, message);
+				free(path);
+				free(message);
+				i++;
+			}
+		}
+		closedir(d);
+	}
 }
 
 void	test_good(void)
 {
-	TEST_ASSERT_EQUAL_INT_MESSAGE(0, parse_map("maps/cheese_maze.cub"), "#1");
+	DIR				*d;
+	struct dirent	*dir;
+	char			*path;
+	char			*message;
+	int				i;
+
+	d = opendir("maps/good/");
+	if (d)
+	{
+		i = 0;
+		while ((dir = readdir(d)) != NULL)
+		{
+			if (dir->d_type == DT_REG)
+			{
+				asprintf(&path, "maps/good/%s", dir->d_name);
+				asprintf(&message, "#%d", i);
+				TEST_ASSERT_EQUAL_INT_MESSAGE(1, parse_map(path), message);
+				// printf("%s - %s\n", path, message);
+				free(path);
+				free(message);
+				i++;
+			}
+		}
+		closedir(d);
+	}
 }
 
 int	test_parse_map(void)
