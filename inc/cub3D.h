e0cb3327b0ce218d8aaf5ba3c273b6298b1e8f06
Dean Ruina <druina@c2r3p1.hive.fi>
2023-10-03 16:04:36 +0300

    base structures
    lead by DeRuina

--- inc/cub3D.h
+++ inc/cub3D.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 10:05:09 by druina            #+#    #+#             */
-/*   Updated: 2023/08/31 14:32:08 by tspoof           ###   ########.fr       */
+/*   Updated: 2023/10/03 16:04:05 by druina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,25 @@
 # define CUB3D_H
 
 # include "libft.h"
-# include "mlx.h"
+# include "mlx42.h"
 # include <fcntl.h>
 # include <math.h>
 # include <stdio.h>
+
+typedef struct s_textures
+{
+	mlx_texture_t *no;
+	mlx_texture_t *so;
+	mlx_texture_t *we;
+	mlx_texture_t *ea;
+} t_textures;
+
+typedef struct	s_map
+{
+	int	floor_color;
+	int	cealing_color;
+	t_textures textures;
+}				t_map;
 
 int	parse_map(char *path);
 
